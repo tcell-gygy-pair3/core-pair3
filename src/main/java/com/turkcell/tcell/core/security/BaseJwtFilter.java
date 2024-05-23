a166e79edd27869a41b37cc754dc4d64103a3a88
SedanurKocal <kocalsedanur@gmail.com>
2024-05-23 20:50:49 +0300

    Deneme

--- src/main/java/com/turkcell/tcell/core/security/BaseJwtFilter.java
+++ src/main/java/com/turkcell/tcell/core/security/BaseJwtFilter.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class BaseJwtFilter extends OncePerRequestFilter {
 
     private final BaseJwtService jwtService;
+    //
 
     public BaseJwtFilter(BaseJwtService jwtService) {
         this.jwtService = jwtService;
